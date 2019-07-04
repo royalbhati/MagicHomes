1f7ec926d5ae69f7261c9f3ec2c1ec3313f49553
Royal Bhati <royal240596@gmail.com>
2019-07-04 10:50:08 +0530

    :sparkles: added signup button:

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -4,62 +4,69 @@ import {withRouter} from 'react-router-dom'
 
 const { Header } = Layout;
  class Nav extends Component {
-  isAuth = () => {
-    if (localStorage.getItem("jwtToken")) {
-      return true;
-    } else {
-      return false;
-    }
-  };
-  logout = () => {
-    localStorage.removeItem("jwtToken");
-    this.props.history.push({
-      pathname: "/",
-      state: {
-        auth: false
-      }
-    });
-  };
-  login = () => {
-    this.props.history.push("/login");
-  };
-  addProperty = () => {
-    this.props.history.push("/addproperty");
-  };
+   isAuth = () => {
+     if (localStorage.getItem("jwtToken")) {
+       return true;
+     } else {
+       return false;
+     }
+   };
+   logout = () => {
+     localStorage.removeItem("jwtToken");
+     this.props.history.push({
+       pathname: "/",
+       state: {
+         auth: false
+       }
+     });
+   };
+   login = () => {
+     this.props.history.push("/login");
+   };
+   signup = () => {
+     this.props.history.push("/signup");
+   };
+   addProperty = () => {
+     this.props.history.push("/addproperty");
+   };
 
-  render() {
+   render() {
+     return (
+       <Header>
+         <div className='brand'>
+           <span
+             style={{ cursor: "pointer" }}
+             onClick={() => this.props.history.push("/")}>
+             Magic Home
+           </span>
+           <span />
 
-    return (
-      <Header>
-        <div className='brand'>
-          <span
-            style={{ cursor: "pointer" }}
-            onClick={() => this.props.history.push("/")}>
-            Magic Home
-          </span>
-          <span />
-
-          <span>
-            {this.isAuth() ? (
-              <Button type='danger' onClick={this.logout}>
-                Logout
-              </Button>
-            ) : (
-              <Button type='danger' onClick={this.login}>
-                Login
-              </Button>
-            )}
-          </span>
-        </div>
-        <div style={{ textAlign: "center" }}>
-          {this.props.role === "admin" && this.isAuth() ? (
-            <Button type='primary' onClick={this.addProperty}>
-              Add New Property
-            </Button>
-          ) : null}
-        </div>
-      </Header>
-    );
-  }
-}
+           <span>
+             {this.isAuth() ? (
+               <Button type='danger' onClick={this.logout}>
+                 Logout
+               </Button>
+             ) : (
+               <span>
+               <Button type='danger' onClick={this.login}>
+                 Login
+               </Button>
+               <Button type='danger' style={{"marginLeft":"10px"}} onClick={this.signup}>
+                 Signup
+               </Button>
+               </span>
+             )}
+           </span>
+         </div>
+         <div style={{ textAlign: "center" }}>
+           {this.props.role === "admin" && this.isAuth() ? (
+             <Button type='primary' onClick={this.addProperty}>
+               Add New Property
+             </Button>
+           ) : null}
+         </div>
+       </Header>
+     );
+   }
+ }
 export default withRouter(Nav);
