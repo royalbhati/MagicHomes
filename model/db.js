2e122b07493ae79baf1a2aea66700abc63f10c76
Royal Bhati <royal240596@gmail.com>
2019-07-08 13:36:34 +0530

    :sparkles:

--- model/db.js
+++ model/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const url ="mongodb+srv://royal:royal123@cluster0-uo6jd.mongodb.net/syncnote?retryWrites=true"
+const url =process.env.MONGO_URL
 // const url ="mongodb://127.0.0.1:27017/test"
 mongoose.connect(url,{useNewUrlParser: true})
 .then(()=>{
